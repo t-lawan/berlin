0e29c9c821463e678fe04f8aac67f330610e8aec
akinsola <thomas.lawanson@gmail.com>
2019-07-12 10:08:40 +0200

    removed default header contents

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,26 @@
-import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styled from 'styled-components';
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+const HeaderWrapper = styled.header`
+  display: flex;
+  position: fixed;
+  /* top: 0; */
+  padding: 0.5 auto ;
+  height: 7.5%;
+  width: 100%;
+  border-bottom: 1px solid black;
+`;
+
+const Header = (props) => (
+  <HeaderWrapper>
+    {props.children}
+  </HeaderWrapper>
 )
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
+  children: PropTypes.node.isRequired
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
-export default Header
+export default Header;
