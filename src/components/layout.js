74b1931eca764cf587ef022ac019b97c5c53b415
akinsola <thomas.lawanson@gmail.com>
2019-07-12 10:13:19 +0200

    changed layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,52 +1,27 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import { createGlobalStyle } from "styled-components"
+import LanguageController from "./languagecontroller"
+import Columns from "./columns"
 
-import Header from "./header"
-import "./layout.css"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
+const GlobalStyle = createGlobalStyle`
+    html,
+    body {
+      width: 100%;
+      height: 100%;
+      margin: 0;
       }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
+`
+const Layout = ({ children }) => (
+  <div>
+    <GlobalStyle />
+    <Columns>{children}</Columns>
+  </div>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
 
-export default Layout
+export default Layout;
