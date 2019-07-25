02852722a6037dd8c19007956efafe143bc50731
akinsola <thomas.lawanson@gmail.com>
2019-07-25 10:08:54 +0100

    added gatsby capabilites

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,40 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require(`path`)
+const slash = require(`slash`)
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage, createRedirect } = actions
+
+  const result = await graphql(`
+    {
+      allWordpressPage {
+        edges {
+          node {
+            id
+            slug
+            type
+            title
+            content
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) {
+    throw new Error(result.errors)
+  }
+
+  const allWordpressPage = result.data.allWordpressPage;
+  const pageTemplate = path.resolve(`./src/templates/page.js`)
+  allWordpressPage.edges.forEach(edge => {
+      createPage({
+          path: edge.node.slug,
+          component: slash(pageTemplate),
+          context: edge.node
+      });
+  });
+}
