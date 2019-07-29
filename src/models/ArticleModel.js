4e9e5336d7d4f225aadbaaec399ff3082c771467
akinsola <thomas.lawanson@gmail.com>
2019-07-29 12:32:35 +0100

    created article model

---
+++ src/models/ArticleModel.js
@@ -0,0 +1,10 @@
+export class ArticleModel {
+    constructor (id, slug, en, de, experience, related_articles) {
+        this.id = id;
+        this.slug = slug;
+        this.en = en;
+        this.de = de;
+        this.experience = experience;
+        this.related_articles = related_articles;
+    }
+}
