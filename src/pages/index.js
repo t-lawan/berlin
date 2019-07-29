e8737bc89d7a0a65368bdc9497c73e7ab1db9d48
akinsola <thomas.lawanson@gmail.com>
2019-07-29 12:34:40 +0100

    using article list as opposed to dummy hello world

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from "react"
 import Layout from "../components/layout";
 import '@getbase/grid/index.css';
+import ArticleList from "../components/articles/articlelist";
 const IndexPage = () => (
   <Layout>
-    <p> Hello World</p>
+    <ArticleList />
   </Layout>
 )
 
