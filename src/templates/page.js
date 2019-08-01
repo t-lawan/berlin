1520400b63ed87ff97dd7e21e254a8fdb9f82b63
akinsola <thomas.lawanson@gmail.com>
2019-08-01 20:48:02 +0100

    page template is using layout

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,8 +1,9 @@
 import React from "react"
+import Layout from "../components/layout";
 
 export default ({ pageContext }) => (
-  <div>
+  <Layout>
     <h1 dangerouslySetInnerHTML={{ __html: pageContext.title }} />
     <div dangerouslySetInnerHTML={{ __html: pageContext.content }} />
-  </div>
+  </Layout>
 );
