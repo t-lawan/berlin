ec8b1b09da5b24064a3f88609025a47585c8fed5
akinsola <thomas.lawanson@gmail.com>
2019-08-01 20:47:42 +0100

    added  columns to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
 import React from "react"
-import Layout from "../components/layout";
-import '@getbase/grid/index.css';
-import ArticleList from "../components/articles/articlelist";
+import Layout from "../components/layout"
+import "@getbase/grid/index.css"
+import ArticleList from "../components/articles/articlelist"
+import Column from "../components/column"
 const IndexPage = () => (
   <Layout>
-    <ArticleList />
+    <div className="row">
+      <Column className="col-6" rightBorder={true}>
+        <ArticleList />
+      </Column>
+      <Column className="col-6">
+        <h1> Hello</h1>
+      </Column>
+    </div>
   </Layout>
 )
 
