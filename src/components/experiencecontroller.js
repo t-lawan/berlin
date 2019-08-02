b5178f09cc758acd26fc8500a8a62487312dddfa
akinsola <thomas.lawanson@gmail.com>
2019-08-02 12:46:32 +0100

    created experience controller

---
+++ src/components/experiencecontroller.js
@@ -0,0 +1,52 @@
+import React from "react"
+import styled from "styled-components"
+import PropTypes from "prop-types"
+import { connect } from "react-redux"
+import * as actionTypes from "../store/action"
+
+const ExperienceControllerWrapper = styled.div`
+  margin-top: 5em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+const ExperienceButton = styled.h3`
+  margin-bottom: 1em;
+  :hover {
+      cursor: ${props => (props.hover ? "pointer" : "inherit")};
+  }
+  font-weight: ${props => (props.bold ? "bold" : "normal")};
+`
+const ExperienceController = props => {
+  let experiences = [1, 2, 3, 4];
+  experiences = experiences.filter(experience => {
+    return experience !== props.experience;
+  });
+  return (
+    <ExperienceControllerWrapper>
+      <ExperienceButton bold> exp</ExperienceButton>
+      {experiences.map(experience => (
+          <ExperienceButton key={experience} bold hover onClick={() => props.changeExperience(experience)}> {experience}</ExperienceButton>
+      ))}
+    </ExperienceControllerWrapper>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    experience: state.experience,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    changeExperience: experience =>
+      dispatch({ type: actionTypes.CHANGE_EXPERIENCE, experience: experience }),
+  }
+}
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ExperienceController)
