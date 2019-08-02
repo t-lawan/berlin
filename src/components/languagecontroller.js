7978d72511bb566ad73b5fc517cdd1b0eef4e5d6
akinsola <thomas.lawanson@gmail.com>
2019-08-02 12:51:03 +0100

    updated language controller

--- src/components/languagecontroller.js
+++ src/components/languagecontroller.js
@@ -1,31 +1,51 @@
 import React from "react"
 import styled from 'styled-components';
 import PropTypes from "prop-types"
+import {connect} from 'react-redux';
+import * as actionTypes from "../store/action";
 
 const LanguageControllerWrapper = styled.div`
-    /* display: flex;
-    flex-direction: column; */
+    display: flex;
+    flex-direction: column;
     border-bottom: 1px solid black;
-    /* align-items: center;
-    justify-content: center; */
-    /* padding: auto; */
-    /* height: 7.5%; */
+    align-items: center;
+    justify-content: center;
+    padding: 1em;
 `;
 
 const LanguageButton = styled.span`
-
+    margin-bottom: 0.5em;
+    font-weight: ${props => props.bold ? "bold" : "normal"};
+    :hover {
+      cursor: ${props => (props.hover ? "pointer" : "inherit")};
+  }
 `;
 
-const LanguageController = (props) => (
-    <LanguageControllerWrapper>
-        {props.languages.map(language => (
-            <LanguageButton key={language}> {language} </LanguageButton>
+const LanguageController = (props) => {
+    const languages = props.languages;
+    const languageToFunctionWrapper = {
+        EN: () => props.setLanguageToEN(),
+        DE: () => props.setLanguageToDE()
+    }
+    return(
+    <LanguageControllerWrapper>    
+        {Object.keys(languages).map(language => (
+         <LanguageButton key={language} onClick={languageToFunctionWrapper[language]} hover bold={languages[language]}> {language.toLowerCase()}</LanguageButton>
         ))}
     </LanguageControllerWrapper>
-)
+)}
 
-LanguageController.propTypes = {
-    languages: PropTypes.array.isRequired
+
+const mapStateToProps = state => {
+    return {
+        languages: state.languages
+    }
 }
 
-export default LanguageController;
+const mapDispatchToProps = dispatch => {
+    return {
+        setLanguageToEN: () => dispatch({type: actionTypes.SET_LANGUAGE_TO_EN}),
+        setLanguageToDE: () => dispatch({type: actionTypes.SET_LANGUAGE_TO_DE})
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(LanguageController);
