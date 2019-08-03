230732bf7f035bb78216e32acd64fd023bb7a72f
akinsola <thomas.lawanson@gmail.com>
2019-08-03 17:31:14 +0100

    added social media footer

---
+++ src/components/socialmedia/socialmedia.styles.js
@@ -0,0 +1,33 @@
+import styled from "styled-components"
+
+export const SocialMediaWrapper = styled.div`
+    display: flex;
+    position: sticky;
+    bottom: 0;
+    flex-direction: row;
+    background: white;
+    width: 100%;
+    border-top: 1px solid black;
+    
+`
+
+export const SocialMediaText = styled.span`
+    padding: 1em;
+    :hover {
+        cursor: pointer;
+    }
+`;
+
+export const SocialMediaLink = styled.a`
+    padding: 1em;
+    :hover {
+        cursor: pointer;
+    }
+    color: inherit; 
+    text-decoration: none;
+`;
+
+export const NewsletterForm = styled.div`
+`
+
+
