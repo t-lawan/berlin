d85d33ba40aec83e47dc4df902e7a115e6a1f99a
akinsola <thomas.lawanson@gmail.com>
2019-08-04 11:26:48 +0100

    restructured columns to be dynamic

--- src/components/columns/columns.js
+++ src/components/columns/columns.js
@@ -7,33 +7,63 @@ import Header from "../header/header"
 import Navbar from "../navbar/navbar"
 import Jumbotron from "../jumbotron/jumbotron"
 import UpcomingEvents from "../events/upcomingevents"
-import ExperienceController from "../experiencecontroller/experiencecontroller";
-import { ColumnsWrapper } from "./columns.styles";
+import ExperienceController from "../experiencecontroller/experiencecontroller"
+import {
+  ColumnsWrapper,
+  FirstColumnWrapper,
+  StickyHeader,
+} from "./columns.styles"
+import SocialMedia from "../socialmedia/socialmedia"
 
-const Columns = props => (
-  <ColumnsWrapper>
-    <Column rightBorder={true}>
-    </Column>
-    <Column rightBorder={true}>
-      <Header />
-      <Navbar />
-      {props.children}
-    </Column>
+const Columns = props => {
+  let renderedComponents
+  const numberOfColumnsIsTwo = props.numberOfColumnsIsTwo
+  if (numberOfColumnsIsTwo) {
+    renderedComponents = (
+      <FirstColumnWrapper twoColumns>
+        <Column rightBorder={true}>{props.firstColumn} </Column>
+        <Column>{props.secondColumn} </Column>
+      </FirstColumnWrapper>
+    )
+  } else {
+    renderedComponents = (
+      <FirstColumnWrapper twoColumns={false}>
+        <Column>{props.firstColumn} </Column>
+      </FirstColumnWrapper>
+    )
+  }
+  return (
+    <ColumnsWrapper>
+      <Column rightBorder={true}></Column>
+      <Column rightBorder={true}>
+        <StickyHeader>
+          <Header />
+          <Navbar />
+        </StickyHeader>
 
-    <Column rightBorder={true}>
-      <Jumbotron/>
-      <UpcomingEvents />
-    </Column>
-
-    <Column rightBorder={false}>
-      <LanguageController/>
-      <ExperienceController/> 
-    </Column>
-  </ColumnsWrapper>
-)
+        {renderedComponents}
+      </Column>
+      <Column rightBorder={true}>
+        <StickyHeader>
+          <Jumbotron />
+        </StickyHeader>
+        {props.thirdColumn}
+        <SocialMedia />
+      </Column>
+      <Column rightBorder={false}>
+        <LanguageController />
+        <ExperienceController />
+      </Column>
+    </ColumnsWrapper>
+  )
+}
 
 export default Columns
 
 Columns.propTypes = {
   children: PropTypes.node.isRequired,
+  firstColumn: PropTypes.node.isRequired,
+  secondColumn: PropTypes.node,
+  thirdColumn: PropTypes.node,
+  numberOfColumnsIsTwo: PropTypes.bool,
 }
--- src/components/columns/columns.styles.js
+++ src/components/columns/columns.styles.js
@@ -5,3 +5,15 @@ export const ColumnsWrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr 12fr 8fr 1fr;
 `
+
+export const FirstColumnWrapper = styled.div`
+  background-color: #fff;
+  display: grid;
+  grid-template-columns: ${props => props.twoColumns ? "1fr 1fr" : "1fr"};
+`
+
+export const StickyHeader = styled.div`
+  z-index: 300;
+  top: 0;
+  position: sticky;
+`
