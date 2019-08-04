843e91a513c5c8cba4f00ae97ee53f98c7e1b13a
akinsola <thomas.lawanson@gmail.com>
2019-08-04 11:32:51 +0100

    modified jumbotron

--- src/components/jumbotron/jumbotron.js
+++ src/components/jumbotron/jumbotron.js
@@ -4,9 +4,8 @@ import { JumbotronWrapper } from "./jumbotron.styles";
 
 const Jumbotron = props => (
   <JumbotronWrapper className={props.className}>
-    <h5> berlin biennale for contemporary art </h5>
-    <h5> sept7 — oct14, 2019 </h5>
-    <p> experience 1</p>
+    <h1> berlin biennale for contemporary art </h1>
+
   </JumbotronWrapper>
 )
 Jumbotron.propTypes = {
--- src/components/jumbotron/jumbotron.styles.js
+++ src/components/jumbotron/jumbotron.styles.js
@@ -2,4 +2,6 @@ import styled from "styled-components"
 
 export const JumbotronWrapper = styled.div`
   border-bottom: 1px solid black;
+  background: white;
+  padding: 1em;
 `
