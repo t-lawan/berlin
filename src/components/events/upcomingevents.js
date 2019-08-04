853a43c1df883a61267dc729c486cd1f9d376b0b
jelena viskovic <jelena.viskovic@gmail.com>
2019-08-04 14:40:01 +0100

    added event generator and utilising experience and languages from state

--- src/components/events/upcomingevents.js
+++ src/components/events/upcomingevents.js
@@ -1,64 +1,35 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { EventsWrapper, EventItem } from "./upcomingevents.styles";
+import { generateEvents } from "../../models/EventsModel";
+import {connect} from 'react-redux';
+import { getCurrentLanguageString } from "../../utility/helper";
 
-const events = [
-    {
-        id: 1,
-        date: Date.now(),
-        title: 'Art',
-        subtitle: 'Subtitle 1',
-        place: 'Lisbon'
-    },
-    {
-        id: 2,
-        date: Date.now(),
-        title: 'Architecture',
-        subtitle: 'Subtitle 1',
-        place: 'London'
-    },
-    {
-        id: 3,
-        date: Date.now(),
-        title: 'Architecture',
-        subtitle: 'Subtitle 1',
-        place: 'London'
-    },
-    {
-        id: 4,
-        date: Date.now(),
-        title: 'Architecture',
-        subtitle: 'Subtitle 1',
-        place: 'London'
-    },
-    {
-        id: 5,
-        date: Date.now(),
-        title: 'Architecture',
-        subtitle: 'Subtitle 1',
-        place: 'London'
-    },
-    {
-        id: 6,
-        date: Date.now(),
-        title: 'Architecture',
-        subtitle: 'Subtitle 1',
-        place: 'London'
-    }
-]
+let events = generateEvents(10);
 
-
-const UpcomingEvents = props => (
+const UpcomingEvents = props => {
+    const filteredEvents = events.filter((event) => {
+        return event.experience === props.experience
+    });
+    const language = getCurrentLanguageString(props.languages);
+    return (
     <EventsWrapper>
-        {events.map(event => (
+        {filteredEvents.map(event => (
             <EventItem key={event.id}>
-                <p> {event.date}</p>
-                <p> {event.title}</p>
-                <p> {event.subtitle}</p>
-                <p> {event.place}</p>
+                <p> {event.start_date.toString()}</p>
+                <p> {event[language].title}</p>
+                <p> {event[language].subtitle}</p>
+                <p> {event.venue}</p>
             </EventItem>
         ))}
     </EventsWrapper>
-)
+)}
 
-export default UpcomingEvents;
+const mapStateToProps = state => {
+    return {
+        languages: state.languages,
+        experience: state.experience
+    }
+}
+
+export default connect(mapStateToProps, null)(UpcomingEvents);
