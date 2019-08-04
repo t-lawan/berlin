3359f1a545be4206488a562aca544901789a7680
jelena viskovic <jelena.viskovic@gmail.com>
2019-08-04 21:39:05 +0100

    removed unused component

--- src/components/events/upcomingevents.js
+++ src/components/events/upcomingevents.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { EventsWrapper, EventItem } from "./upcomingevents.styles";
 import { generateEvents } from "../../models/EventsModel";
 import {connect} from 'react-redux';
