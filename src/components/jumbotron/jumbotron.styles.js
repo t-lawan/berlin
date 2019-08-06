5a924a42e9f083692c9b12083883afa71dd694c9
akinsola <thomas.lawanson@gmail.com>
2019-08-06 07:44:25 +0100

    added link to jumbotron

--- src/components/jumbotron/jumbotron.styles.js
+++ src/components/jumbotron/jumbotron.styles.js
@@ -1,7 +1,15 @@
 import styled from "styled-components"
-
+import { Link } from "gatsby"
 export const JumbotronWrapper = styled.div`
   border-bottom: 1px solid black;
   background: white;
   padding: 1em;
+`
+
+export const JumbotronHeader = styled(Link)`
+  text-decoration: none;
+  color: black;
+  :hover {
+    cursor: pointer;
+  }
 `
