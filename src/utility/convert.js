2561a8441af37b56294d7fdd84007123bcae4669
akinsola <thomas.lawanson@gmail.com>
2019-08-10 16:53:24 +0100

    added generic converter function

--- src/utility/convert.js
+++ src/utility/convert.js
@@ -1,4 +1,5 @@
 import { NewsModel } from "../models/NewsModel";
+import { EventsModel } from "../models/EventsModel";
 
 export class Convert {
 
@@ -21,6 +22,40 @@ export class Convert {
             articleArray.push(article);
         });
         return articleArray;
+    }
+
+    static toEventModel = (wordpressModel) => {
+        return new EventsModel(
+            wordpressModel.id,
+            wordpressModel.slug,
+            wordpressModel.acf.exp_number,
+            wordpressModel.acf.EN,
+            wordpressModel.acf.DE,
+            wordpressModel.acf.start_date,
+            wordpressModel.acf.end_date,
+            "Venue",
+            wordpressModel.acf.event_documentation,
+            wordpressModel.acf.event_is_free,
+            wordpressModel.acf.event_language,
+            wordpressModel.acf.event_limited_capacity,
+            wordpressModel.acf.thumbnail_image,
+            wordpressModel.acf.participants,
+            wordpressModel.acf.related_resources,
+            wordpressModel.acf.other_event_language
+        )
+    }
+
+    static toModelArray = (wordpressQuery, modelConverter) => {
+        const modelArray = [];
+        wordpressQuery.edges.map(wordpressModel => {
+            let model = modelConverter(wordpressModel.node);
+            modelArray.push(model);
+        });
+        return modelArray;
+    }
+
+    static eventsToCalendarModel = () => {
+        
     }
 
 
