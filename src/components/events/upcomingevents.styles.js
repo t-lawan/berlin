3b0b1b915d97a6d158cb65ea3880bf5947b8d24d
akinsola <thomas.lawanson@gmail.com>
2019-08-10 16:53:43 +0100

    added upcoming event

--- src/components/events/upcomingevents.styles.js
+++ src/components/events/upcomingevents.styles.js
@@ -1,12 +1,13 @@
 import styled from "styled-components"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 
 export const EventsWrapper = styled.div`
-padding: 2em;
-
+  padding: 2em;
 `
 
-export const EventItem = styled.div`
-    padding-top: 1em;
-    border-bottom: 1px solid black;
+export const EventItem = styled(AniLink)`
+  padding-top: 1em;
+  border-bottom: 1px solid black;
+  text-decoration: none;
+  color: black;
 `
-
