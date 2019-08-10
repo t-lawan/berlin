66c5625aeafaae2c1270521532d14d7797bb7821
akinsola <thomas.lawanson@gmail.com>
2019-08-10 16:26:08 +0100

    retrieving events and creating page for each event
    also using different templates for pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -19,6 +19,7 @@ exports.createPages = async ({ graphql, actions }) => {
           node {
             slug
             acf {
+              template
               DE {
                 content
                 title
@@ -31,29 +32,80 @@ exports.createPages = async ({ graphql, actions }) => {
           }
         }
       }
+      allWordpressWpEvents {
+        edges {
+          node {
+            slug
+            id
+            acf {
+              DE {
+                display_time
+                event_subtitle
+                full_description
+                event_title
+                other_event_venue
+                short_calendar_description
+              }
+              EN {
+                event_title
+                display_time
+                event_subtitle
+                full_description
+                other_event_venue
+                short_calendar_description
+              }
+              end_date
+              event_documentation
+              event_is_free
+              event_language
+              event_limited_capacity
+              start_date(formatString: "YYYY.MM.DD")
+              template
+              thumbnail_image
+              exp_number
+              participants
+              related_resources
+              other_event_language
+            }
+          }
+        }
+      }
     }
   `)
 
   if (result.errors) {
     throw new Error(result.errors)
   }
-  const allWordpressPage = result.data.allWordpressPage;
+  const { allWordpressPage, allWordpressWpEvents } = result.data
 
-  const pageTemplate = path.resolve(`./src/templates/page.js`);
-  // const articleTemplate = path.resolve(`./src/templates/article.js`);
+  const pageTemplate = path.resolve(`./src/templates/page.js`)
+  const calendarTemplate = path.resolve(`./src/templates/calendar-template.js`)
+
   allWordpressPage.edges.forEach(edge => {
+    let template
+    switch (edge.node.acf.template) {
+      case "calendar":
+        template = calendarTemplate
+        break
+      case "default":
+        template = pageTemplate
+        break
+      default:
+        template = pageTemplate
+    }
     createPage({
       path: edge.node.slug,
-      component: slash(pageTemplate),
+      component: slash(template),
       context: edge.node,
-    });
-  });
+    })
+  })
 
-  // allWordpressWpArticle.edges.forEach(edge => {
-  //   createPage({
-  //     path: "/article/" +  edge.node.slug,
-  //     component: slash(articleTemplate),
-  //     context: edge.node
-  //   });
-  // });
+  const eventTemplate = path.resolve(`./src/templates/event.js`)
+  allWordpressWpEvents.edges.forEach(edge => {
+    createPage({
+      path: "/event/" + edge.node.slug,
+      component: slash(eventTemplate),
+      context: edge.node,
+    })
+  })
 }
