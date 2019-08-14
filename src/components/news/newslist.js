3fce71cd528e480589b9e141d4a64b7be5514c22
akinsola <thomas.lawanson@gmail.com>
2019-08-14 23:48:39 +0100

    utilising news template

--- src/components/news/newslist.js
+++ src/components/news/newslist.js
@@ -1,34 +1,37 @@
 import React from "react"
-import { getCurrentLanguageString } from "../../utility/helper";
-import { generateNewsArticles } from "../../models/NewsModel";
-import {connect} from 'react-redux';
-import { NewsListWrapper, NewsItem} from "./newslist.styles";
-
-let newsArray = generateNewsArticles(10);
+import { getCurrentLanguageString } from "../../utility/helper"
+import { generateNewsArticles } from "../../models/NewsModel"
+import { connect } from "react-redux"
+import { NewsListWrapper } from "./newslist.styles"
+import NewsItem from './news-item';
+let newsArray = generateNewsArticles(50)
 
 const NewsList = props => {
-  const filteredNews = newsArray.filter((news) =>{
+  const filteredNews = newsArray.filter(news => {
     return news.experience === props.experience
-  });
-  const language = getCurrentLanguageString(props.languages);
+  })
+  const language = getCurrentLanguageString(props.languages)
   return (
-  <NewsListWrapper>
-    
+    <NewsListWrapper>
       {filteredNews.map(news => (
-       <NewsItem key={news.id}>
-         <p> {news[language].title}</p>
-         <p> {news[language].description}</p>
-       </NewsItem>
+        <NewsItem
+          key={news.id}
+          title={news[language].title}
+          description={news[language].description}
+        />
       ))}
-  
     </NewsListWrapper>
-  )}
+  )
+}
 
 const mapStateToProps = state => {
   return {
-      languages: state.languages,
-      experience: state.experience
+    languages: state.languages,
+    experience: state.experience,
   }
 }
 
-export default connect(mapStateToProps, null) (NewsList);
+export default connect(
+  mapStateToProps,
+  null
+)(NewsList)
--- src/components/news/newslist.styles.js
+++ src/components/news/newslist.styles.js
@@ -1,11 +1,5 @@
 import styled from "styled-components"
 
-
-
 export const NewsListWrapper = styled.div`
   padding: 1em;
-`
-export const NewsItem = styled.div`
-  padding-top: 1em;
-  border-bottom: 1px solid black;
 `
