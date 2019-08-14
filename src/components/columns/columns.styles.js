30426c634e4c384a7df444fc14d2ce5a496110fe
akinsola <thomas.lawanson@gmail.com>
2019-08-14 23:51:57 +0100

    created media queries

--- src/components/columns/columns.styles.js
+++ src/components/columns/columns.styles.js
@@ -1,17 +1,19 @@
-import styled from "styled-components"
+import styled, { css } from "styled-components"
+import { changeGridToOneRow } from "../../index.styles";
+
 
 export const ColumnsWrapper = styled.div`
   background-color: #fff;
   display: grid;
-  grid-template-columns: 1fr 12fr 8fr 1fr;
+  grid-template-columns: 1fr 10fr 7fr 1fr;
+  ${changeGridToOneRow}
 `
 
 export const FirstColumnWrapper = styled.div`
   background-color: #fff;
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: ${props => (props.twoColumns ? "1fr 1fr" : "1fr")};
 
-  grid-template-columns: ${props => props.twoColumns ? "1fr 1fr" : "1fr"};
 `
 
 export const StickyHeader = styled.div`
