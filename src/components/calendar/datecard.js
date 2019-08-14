7008d38afff58c35000c232b6d60f8e07c69ab6a
akinsola <thomas.lawanson@gmail.com>
2019-08-14 23:48:59 +0100

    created styles sheet and migrated styles

--- src/components/calendar/datecard.js
+++ src/components/calendar/datecard.js
@@ -1,71 +1,27 @@
 import React from "react"
 import PropTypes from "prop-types"
-import styled from "styled-components"
-import { EventsModel } from "../../models/EventsModel"
 import { connect } from "react-redux"
-import { getCurrentLanguageString } from "../../utility/helper"
 import { DateManager } from "../../utility/date"
-import moment from "moment"
+import EventCard from "./eventcard";
+import { DateCardWrapper, CurrentDate, DateText, DayMonthText, MonthHeading } from "./datecard.styles";
 
-const DateCardWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  border-bottom: 0.05em solid black;
-  border-right: 0.05em solid black;
-  min-height: 400px;
-  height: auto;
-  background-color: ${props => (props.addColour ? "#ADEBAD" : "inherit")};
-`
-const EventCardWrapper = styled.div`
-  border-bottom: 0.05em solid black;
-  height: auto;
-  padding: 0.25em 0.25em;
-`
-
-const CurrentDate = styled.div`
-  border-bottom: 0.05em solid black;
-  padding: 0.15em 0.5em;
-`
-
-const EventText = styled.p`
-  font-size: x-small;
-`
-
-const EventHeading = styled.strong`
-  font-size: x-small;
-`
-
-const DateText = styled.strong`
-  font-size: xx-large;
-  padding-right: 0.5em;
-`
-
-const DayMonthText = styled.span`
-  font-size: small;
-`
-const MonthHeading = styled.h1`
-  padding: 0.25em 0.5em;
-`
 
 const DateCard = props => {
-  const language = getCurrentLanguageString(props.languages)
   let renderComponents;
   if (props.containsEvents) {
-    const monthDay = DateManager.createMonthDayString(props.day, props.month)
-    const date = DateManager.createDatetring(props.day, props.month)
+    const monthDay = DateManager.createMonthDayString(props.day, props.month);
+    const date = DateManager.createDatetring(props.day, props.month);
+    const events = props.events.sort((a,b) => {
+      return a.start_time.charAt(0) - b.start_time.charAt(0);
+    });
     renderComponents = (
       <DateCardWrapper id={`date-${DateManager.createDateClass(props.day, props.month)}`}>
         <CurrentDate>
           <DateText>{date}</DateText>
           <DayMonthText> {monthDay.toLocaleLowerCase()} </DayMonthText>
         </CurrentDate>
-        {props.events.map(event => (
-          <EventCardWrapper key={event.id}>
-            <EventHeading>{event[language].display_time}</EventHeading>
-            <EventText>Talk</EventText>
-            <EventText>{event.venue}</EventText>
-            <EventText>{event[language].short_calendar_description}</EventText>
-          </EventCardWrapper>
+        {events.map(event => (
+          <EventCard key={event.id} event={event}/>
         ))}
       </DateCardWrapper>
     )
@@ -77,7 +33,6 @@ const DateCard = props => {
       </DateCardWrapper>
     )
   }
-
   return renderComponents
 }
 
