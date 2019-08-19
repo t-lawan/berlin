c78af73dcf48444b354b61f937ca36c3f2e85108
akinsola <thomas.lawanson@gmail.com>
2019-08-20 00:17:18 +0100

    migrated global style to index page

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,35 +1,24 @@
 import React from "react"
 import PropTypes from "prop-types"
-import styled, { createGlobalStyle } from "styled-components"
+import styled from "styled-components"
 import Columns from "../columns/columns"
-import NewsList from "../news/newslist"
-import UpcomingEvents from "../events/upcomingevents"
+import { GlobalStyle } from "../../index.styles";
 
-const GlobalStyle = createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css?family=Raleway:400,400i&display=swap');
-    html,
-    body {
-      width: 100vw;
-      width: 100%;
-      height: 100%;
-      margin: 0 auto;
-      font-family: 'Raleway', sans-serif;
-      }
-`
 
 const LayoutWrapper = styled.div`
   width: 100vw;
   width: 100%;
 `
 
-const Layout = (props) => (
+const Layout = props => (
   <LayoutWrapper>
     <GlobalStyle />
     <Columns
       firstColumn={props.firstColumn}
       secondColumn={props.secondColumn}
-      thirdColumn={props.thirdColumn} 
-      numberOfColumnsIsTwo={props.numberOfColumnsIsTwo} />
+      thirdColumn={props.thirdColumn}
+      numberOfColumnsIsTwo={props.numberOfColumnsIsTwo}
+    />
   </LayoutWrapper>
 )
 
@@ -37,7 +26,7 @@ Layout.propTypes = {
   firstColumn: PropTypes.node.isRequired,
   secondColumn: PropTypes.node,
   thirdColumn: PropTypes.node.isRequired,
-  numberOfColumnsIsTwo: PropTypes.bool.isRequired
+  numberOfColumnsIsTwo: PropTypes.bool.isRequired,
 }
 
 export default Layout
--- src/index.styles.js
+++ src/index.styles.js
@@ -1,4 +1,17 @@
-import styled, { css } from "styled-components"
+import styled, { css, createGlobalStyle } from "styled-components"
+
+export const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Raleway:400,400i&display=swap');
+@import url('https://use.typekit.net/xcm3ryn.css');
+html,
+body {
+  width: 100vw;
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  font-family: 'ag-book-pro', sans-serif;
+  }
+`
 
 export const size = {
   mobileS: "320px",
@@ -10,9 +23,8 @@ export const size = {
   desktop: "2560px",
 }
 
-
 export const hideDisplayForTablet = css`
-  ${'' /* display: ${props => (props.hideInMobile ? "inherit" : "inherit")}; */}
+  ${"" /* display: ${props => (props.hideInMobile ? "inherit" : "inherit")}; */}
   @media (max-width: ${size.tablet}) {
     display: ${props => (props.hideInMobile ? "none" : "inherit")};
   }
@@ -33,6 +45,6 @@ export const changeGridToOneRow = css`
 
 export const changeGridToThreeEqualRows = css`
   @media (max-width: ${size.tablet}) {
-    grid-template-columns: repeat(3, 1fr)
+    grid-template-columns: repeat(3, 1fr);
   }
 `
