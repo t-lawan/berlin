7f7577692a9f1d7b6fe64266c6215133e95849ce
akinsola <thomas.lawanson@gmail.com>
2019-08-23 11:14:54 +0100

    updated exhibition model

--- src/models/ExhibitionModel.js
+++ src/models/ExhibitionModel.js
@@ -1,59 +1,67 @@
 const faker = require("faker")
-const moment  = require('moment');
+const moment = require("moment")
 
 export class ExhibitionModel {
-    constructor(
-        id,
-        slug,
-        experience,
-        EN,
-        DE,
-        start_time,
-        start_date,
-        end_date,
-        venue,
-        floor_plan,
-        participants
-      ) {
-        this.id = id
-        this.slug = slug
-        this.experience = experience
-        this.EN = EN
-        this.DE = DE
-        this.start_time = start_time
-        this.start_date = start_date
-        this.end_date = end_date
-        this.venue = venue
-        this.floor_plan = floor_plan
-        this.participants = participants
-      }
+  constructor(
+    id,
+    slug,
+    experience,
+    EN,
+    DE,
+    start_time,
+    start_date,
+    end_date,
+    venue,
+    floor_plan,
+    participants
+  ) {
+    this.id = id
+    this.slug = slug
+    this.experience = experience
+    this.EN = EN
+    this.DE = DE
+    this.start_time = start_time
+    this.start_date = start_date
+    this.end_date = end_date
+    this.venue = venue
+    this.floor_plan = floor_plan
+    this.participants = participants
+  }
 }
 
 export const generateExhibitions = numberOfExhibitions => {
-    const exhibitionsArray = []
-    for (let i = 0; i < numberOfExhibitions; i++) {
-      const language = {
-        title: faker.random.word(),
-        subtitle: faker.random.word(10),
-        description: faker.random.word(20)
-      };
-  
-      const exhibition = new ExhibitionModel(
-        i + 1,
-        faker.random.word(),
-        Math.floor(Math.random() * 4) + 1,
-        language,
-        language,
-        `${faker.random.number(1, 2)} pm`,
-        moment(faker.date.between('2019-08-01', '2019-12-31'), "YYYY-MM-DD HH:mm Z").format('Ymd'),
-        moment(faker.date.between('2019-08-01', '2019-12-31'), "YYYY-MM-DD HH:mm Z").format('Ymd'),
-        faker.random.word(3),
-        null,
-        null
-        
-      )
-      exhibitionsArray.push(exhibition)
+  const exhibitionsArray = []
+  for (let i = 0; i < numberOfExhibitions; i++) {
+    const language = {
+      title: faker.random.word(),
+      subtitle: faker.random.word(10),
+      short_calendar_description: faker.random.words(10),
+      full_description: faker.random.word(20),
+      time: `${faker.random.number(1, 2)} pm`,
+      display_time: `${Math.floor(Math.random() * 4) + 1} pm`
     }
-  
-    return exhibitionsArray
+
+    const exhibition = new ExhibitionModel(
+      i + 1,
+      faker.random.word(),
+      Math.floor(Math.random() * 4) + 1,
+      language,
+      language,
+      `${faker.random.number(1, 2)} pm`,
+      moment(
+        faker.date.between("2019-08-01", "2019-12-31"),
+        "YYYY-MM-DD HH:mm Z"
+      ).format("YYYYMMDD"),
+      moment(
+        faker.date.between("2019-08-01", "2019-12-31"),
+        "YYYY-MM-DD HH:mm Z"
+      ).format("YYYYMMDD"),
+      faker.random.word(3),
+      null,
+      null
+    )
+    exhibitionsArray.push(exhibition)
   }
+
+  return exhibitionsArray
+}
