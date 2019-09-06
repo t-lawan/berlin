3a9f4d89e64407edaf00b5708301cbb42987efa8
akinsola <thomas.lawanson@gmail.com>
2019-09-06 19:55:27 +0100

    changed path

--- src/components/events/upcomingevents.js
+++ src/components/events/upcomingevents.js
@@ -17,7 +17,7 @@ const UpcomingEvents = props => {
     <EventsWrapper>
       {filteredEvents.map(event => (
         <EventItem key={event.id}>
-          <EventLink to={createPath(language,`/event/${event.slug}`)}>
+          <EventLink to={createPath(language,`event/${event.slug}`)}>
             <p> {event.start_date.toString()}</p>
             <p> {event[language].title}</p>
             <p> {event[language].subtitle}</p>
