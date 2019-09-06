7881b528e9f449e7c27696e0a2756a3a2062efdb
akinsola <thomas.lawanson@gmail.com>
2019-09-06 22:57:01 +0100

    added venues to store

--- src/store/action.js
+++ src/store/action.js
@@ -8,5 +8,7 @@ export const SET_EVENTS = "SET_EVENTS";
 export const SET_NEWS = "SET_NEWS";
 export const SET_EXHIBITIONS = "SET_EXHIBITIONS";
 export const SET_PARTICIPANTS = "SET_PARTICIPANTS";
+export const SET_VENUES = "SET_VENUES";
+
 
 export const IS_LOADED = "IS_LOADED";
