f5cfa6574ab81ed607b2d722d9c1b8eee0ef427e
akinsola <thomas.lawanson@gmail.com>
2019-09-12 20:37:24 +0100

    added animation to columns

--- src/components/columns/columns.js
+++ src/components/columns/columns.js
@@ -1,70 +1,84 @@
 import React from "react"
-import styled from "styled-components"
 import PropTypes from "prop-types"
 import Column from "./column"
 import LanguageController from "../languagecontroller/languagecontroller"
 import Header from "../header/header"
 import Navbar from "../navbar/navbar"
 import Jumbotron from "../jumbotron/jumbotron"
-import UpcomingEvents from "../events/upcomingevents"
 import ExperienceController from "../experiencecontroller/experiencecontroller"
+import { Animated } from "react-animated-css"
+import { connect } from "react-redux"
+
 import {
   ColumnsWrapper,
   FirstColumnWrapper,
   StickyHeader,
+  AnimatedColumn,
 } from "./columns.styles"
 import SocialMedia from "../socialmedia/socialmedia"
-import Logo from "../logo/logo";
+import Logo from "../logo/logo"
 
-const Columns = props => {
-  let renderedComponents
-  const numberOfColumnsIsTwo = props.numberOfColumnsIsTwo
-  if (numberOfColumnsIsTwo) {
-    renderedComponents = (
-      <FirstColumnWrapper twoColumns>
-        <Column rightBorder={true}>{props.firstColumn} </Column>
-        <Column>{props.secondColumn} </Column>
-      </FirstColumnWrapper>
-    )
-  } else {
-    renderedComponents = (
-      <FirstColumnWrapper twoColumns={false}>
-        <Column>{props.firstColumn} </Column>
-      </FirstColumnWrapper>
+class Columns  extends React.Component {
+  renderedComponents;
+  numberOfColumnsIsTwo = this.props.numberOfColumnsIsTwo;
+
+  render(){
+
+    if (this.numberOfColumnsIsTwo) {
+      this.renderedComponents = (
+        <FirstColumnWrapper twoColumns>
+          <Column rightBorder={true}>{this.props.firstColumn} </Column>
+          <Column>{this.props.secondColumn} </Column>
+        </FirstColumnWrapper>
+      )
+    } else {
+      this.renderedComponents = (
+        <FirstColumnWrapper twoColumns={false}>
+          <Column>{this.props.firstColumn} </Column>
+        </FirstColumnWrapper>
+      )
+    }
+  
+    
+    return (
+      <ColumnsWrapper>
+        <Column rightBorder={true} hideInMobile>
+          <ExperienceController left={true} />
+        </Column>
+        <AnimatedColumn
+          animationIn={this.props.experience_transition.animationIn}
+          animationOut={this.props.experience_transition.animationOut}
+          isVisible={this.props.experience_transition.isVisible}
+          animateOnMount={false}
+          animationInDuration={500}
+        >
+          <Column rightBorder={true}>
+            <StickyHeader>
+              <Jumbotron showInMobile />
+              <Header hideInMobile />
+              <Navbar />
+            </StickyHeader>
+  
+            {this.renderedComponents}
+          </Column>
+          <Column rightBorder={true} hideInMobile>
+            <StickyHeader>
+              <Jumbotron />
+            </StickyHeader>
+            <Logo />
+            {this.props.thirdColumn}
+            <SocialMedia />
+          </Column>
+        </AnimatedColumn>
+  
+        <Column rightBorder={false} hideInMobile>
+          <LanguageController />
+          <ExperienceController left={false} />
+        </Column>
+      </ColumnsWrapper>
     )
   }
-  return (
-    <ColumnsWrapper>
-      <Column rightBorder={true} hideInMobile>
-        <ExperienceController left={true} />
-      </Column>
-
-      <Column rightBorder={true}>
-        <StickyHeader>
-          <Jumbotron showInMobile/>
-          <Header hideInMobile/>
-          <Navbar />
-        </StickyHeader>
-
-        {renderedComponents}
-      </Column>
-      <Column rightBorder={true} hideInMobile>
-        <StickyHeader>
-          <Jumbotron />
-        </StickyHeader>
-        <Logo />
-        {props.thirdColumn}
-        <SocialMedia />
-      </Column>
-      <Column rightBorder={false} hideInMobile>
-        <LanguageController />
-        <ExperienceController left={false} />
-      </Column>
-    </ColumnsWrapper>
-  )
 }
-
-export default Columns
 
 Columns.propTypes = {
   firstColumn: PropTypes.node.isRequired,
@@ -72,3 +86,13 @@ Columns.propTypes = {
   thirdColumn: PropTypes.node,
   numberOfColumnsIsTwo: PropTypes.bool,
 }
+
+const mapStateToProps = state => {
+  return {
+    experience_transition: state.experience_transition,
+  }
+}
+export default connect(
+  mapStateToProps,
+  null
+)(Columns)
--- src/components/columns/columns.styles.js
+++ src/components/columns/columns.styles.js
@@ -1,19 +1,28 @@
 import styled, { css } from "styled-components"
-import { changeGridToOneRow } from "../../index.styles";
+import { changeGridToOneRow } from "../../index.styles"
+import { Animated } from "react-animated-css"
 
+export const AnimatedColumn = styled(Animated)`
+  height: 100%;
+  height: 100vh;
+  width: inherit;
+  overflow-y: auto;
+  overflow-x: hidden;
+  display: grid;
+  grid-template-columns: 5fr 3fr;
+  ${changeGridToOneRow}
 
+`
 export const ColumnsWrapper = styled.div`
   background-color: #fff;
   display: grid;
-  grid-template-columns: 1fr 10fr 7fr 1fr;
-  ${changeGridToOneRow}
+  grid-template-columns: 1fr 20fr 1fr;
 `
 
 export const FirstColumnWrapper = styled.div`
   background-color: #fff;
   display: grid;
   grid-template-columns: ${props => (props.twoColumns ? "1fr 1fr" : "1fr")};
-
 `
 
 export const StickyHeader = styled.div`
