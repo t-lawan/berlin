51466870f222d9475e30b79342472e9d070b228a
akinsola <thomas.lawanson@gmail.com>
2019-09-13 20:50:57 +0100

    crrated data privacy

---
+++ src/components/data-privacy/data-privacy.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { connect } from "react-redux"
+import styled from "styled-components"
+
+
+const DataPrivacy = (props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
