91add159276e8be0ea5b83c860d7670ded226d50
akinsola <thomas.lawanson@gmail.com>
2019-09-28 16:21:34 +0100

    updated imagegallery

--- src/partials/ImageGalleryResource.js
+++ src/partials/ImageGalleryResource.js
@@ -5,18 +5,50 @@ import { getCurrentLanguageString } from "../utility/helper"
 import { getDocuments } from "../store/selector"
 import styled from "styled-components"
 import { Animated } from "react-animated-css"
-import ImageResource from "./ImageResource";
+import ImageResource from "./ImageResource"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faLongArrowAltLeft,
+  faLongArrowAltRight,
+} from "@fortawesome/free-solid-svg-icons"
 
-export const NavigatingButton = styled.span`
+export const NavigatingButton = styled.div`
   color: ${props => (props.show ? "black" : "grey")};
   cursor: ${props => (props.show ? "pointer" : "inherit")};
-  font-size: 1.5em;
+  font-size: 2rem;
+  /* display:inline-block; */
+  /* text-align: center; */
+  margin: auto;
+  position: relative;
+  z-index: 1000;
+  width: 50%;
 `
 
 export const NavigationSpace = styled.div`
+  position: relative;
+`
+export const NavigationButtons = styled.div`
   position: absolute;
-  width: 50%;
-  /* background: red; */
+  display: flex;
+  flex-direction: row;
+  height: 100%;
+  width: 100%;
+  padding: 1rem;
+  visibility: hidden;
+  ${NavigationSpace}:hover & {
+    visibility:visible;
+  }
+`
+
+export const AnimatedSpace = styled(Animated)`
+  z-index: 100;
+`
+
+export const NavigationIcon = styled(FontAwesomeIcon)`
+  width: 0.25rem;
+  align-content: center;
+  margin: auto;
+  float: ${props => (props.floatRight ? "right" : "left")};
 `
 
 export const GalleryImage = styled.img``
@@ -33,28 +65,10 @@ class ImageGalleryResource extends React.Component {
     animationIn: "slideInLeft",
     animationOut: "slideOutRight",
     isVisible: true,
-    isTransitioning: false
+    isTransitioning: false,
   }
   imageResources
   language
-  time = 1250
-
-  nextImage = () => {
-    if (this.state.index + 1 < this.imageResources.length && !this.state.isTransitioning) {
-
-      this.setState({
-        isVisible: false,
-      })
-
-      this.setState({
-        index: this.state.index + 1,
-        animationIn: "slideInLeft",
-        animationOut: "slideOutRight",
-        isTransitioning: true
-      })
-    }
-  }
-
   previousImage = () => {
     if (this.state.index !== 0 && !this.state.isTransitioning) {
       this.setState({
@@ -64,58 +78,79 @@ class ImageGalleryResource extends React.Component {
         index: this.state.index - 1,
         animationIn: "slideInRight",
         animationOut: "slideOutLeft",
-        isTransitioning: true
+        isTransitioning: true,
       })
     }
   }
+  nextImage = () => {
+    if (
+      this.state.index + 1 < this.imageResources.length &&
+      !this.state.isTransitioning
+    ) {
+      this.setState({
+        isVisible: false,
+      })
 
+      this.setState({
+        index: this.state.index + 1,
+        animationIn: "slideInLeft",
+        animationOut: "slideOutRight",
+        isTransitioning: true,
+      })
+    }
+  }
   isLastImage = () => {
     return this.imageResources.length === this.state.index + 1 ? true : false
   }
   setVisibleToTrue = () => {
-    // setTimeout(() => {
-      this.setState({ isVisible: true, isTransitioning: false })
-    // }, 1000)
+    this.setState({ isVisible: true, isTransitioning: false })
   }
-
   isFirstImage = () => {
     if (this.state.index === 0) {
       return true
     }
     return false
   }
+
+  changeImage = event => {
+    console.log(event)
+    console.log(1)
+  }
   render() {
     this.language = getCurrentLanguageString(this.props.languages)
     this.imageResources = getDocuments(this.props.documents, this.props.ids)
     return (
       <>
-        <div>
-          <NavigatingButton
-            show={!this.isFirstImage()}
-            onClick={() => this.previousImage()}
+        <NavigationSpace>
+          <NavigationButtons>
+            <NavigatingButton
+              show={!this.isFirstImage()}
+              onClick={() => this.previousImage()}
+            >
+              <NavigationIcon icon={faLongArrowAltLeft} />
+            </NavigatingButton>
+            <NavigatingButton
+              show={!this.isLastImage()}
+              onClick={() => this.nextImage()}
+            >
+              <NavigationIcon icon={faLongArrowAltRight} floatRight />
+            </NavigatingButton>
+          </NavigationButtons>
+
+          <AnimatedSpace
+            animationIn={this.state.animationIn}
+            animationOut={this.state.animationOut}
+            isVisible={this.state.isVisible}
+            animateOnMount={false}
           >
-            &lt;
-          </NavigatingButton>
-          <NavigatingButton
-            show={!this.isLastImage()}
-            onClick={() => this.nextImage()}
-          >
-            &gt;
-          </NavigatingButton>
-        </div>
-        <Animated
-          animationIn={this.state.animationIn}
-          animationOut={this.state.animationOut}
-          isVisible={this.state.isVisible}
-          animateOnMount={false}
-        >
-          <NavigationSpace></NavigationSpace>
-          <ImageResource
-            onLoad={() => this.setVisibleToTrue()}
-            id={this.props.ids[this.state.index]}
-            withCaption={false}
-          />
-        </Animated>
+            <ImageResource
+              onClick={event => this.changeImage(event)}
+              onLoad={() => this.setVisibleToTrue()}
+              id={this.props.ids[this.state.index]}
+              withCaption={false}
+            />
+          </AnimatedSpace>
+        </NavigationSpace>
       </>
     )
   }
