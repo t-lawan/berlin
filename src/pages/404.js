c456175686e2311b23bfbcb4575024aabe79a16f
akinsola <thomas.lawanson@gmail.com>
2019-10-03 22:08:45 +0100

    updated 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,30 @@
 import React from "react"
-import Layout from "../components/layout/layout";
-import SEO from "../components/seo/seo";
+import Layout from "../components/layout/layout"
+import SEO from "../components/seo/seo"
+import UpcomingEvents from "../components/events/upcomingevents"
+import { PageWrapper } from "../templates/page.styles";
+import { Color } from "../index.styles";
 
-
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = () => {
+  let renderComponent = (
+    <PageWrapper colour={Color.red}>
+      <SEO
+        title={`404: Not found`}
+        description={`404: Not found`}
+        lang={'en'}
+      />
+      <p> Page not found.</p>
+      <p>You just hit a route that doesn&#39;t exist...</p>
+    </PageWrapper>
+  )
+  return (
+    <Layout
+      firstColumn={renderComponent}
+      secondColumn={renderComponent}
+      numberOfColumnsIsTwo={false}
+      thirdColumn={<UpcomingEvents />}
+    />
+  )
+}
 
 export default NotFoundPage
