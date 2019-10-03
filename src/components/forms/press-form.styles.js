d9ade2b6c01fb7ee6216c5b2b72b3a20139332ad
akinsola <thomas.lawanson@gmail.com>
2019-10-03 22:09:04 +0100

    added form

---
+++ src/components/forms/press-form.styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+import { Color } from '../../index.styles';
+
+export const PressFormWrapper = styled.div`
+  padding: 1em 0em;
+`
+
+export const PressFormError = styled.p`
+  padding: 0.5rem 0rem;
+  color: ${Color.red};
+`
+
+export const PressFormInput = styled.input`
+  font-size: 1em;
+  border-radius: 0;
+  border: 1px solid black;
+   padding: 0.5em 0.5em;
+   width: 19rem;
+`
