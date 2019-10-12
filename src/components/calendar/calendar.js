a36e3c9eaf650a841470bd7d3ee9188c3a7ba309
akinsola <thomas.lawanson@gmail.com>
2019-10-12 11:39:53 +0100

    updated calendar to render between months of exhibition

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -6,12 +6,37 @@ import { Convert } from "../../utility/convert"
 import { CalendarModel } from "../../models/CalendarModel"
 import MonthCards from "./monthcards"
 import { CalendarWrapper } from "./calendar.styles"
+import { DateManager } from "../../utility/date";
 
 let events = []
 let exhibitions = []
 const Calendar = props => {
   const calendar = props.calendar
-  const months = Object.keys(calendar)
+  let months = Object.keys(calendar)
+  let exhibition = props.exhibitions.find((item) => parseInt(item.experience) === props.experience);
+
+  if(exhibition) {
+    let start_date = {
+      month: DateManager.get(exhibition.start_date, "month") + 1,
+      year: DateManager.get(exhibition.start_date, "year") 
+    }
+  
+    let end_date = {
+      month: DateManager.get(exhibition.end_date, "month") + 1,
+      year: DateManager.get(exhibition.end_date, "year") 
+    }
+
+    months = months.filter((item) => {
+      let month = parseInt(item.split('-')[1]);
+      let year = parseInt(item.split('-')[0]);
+      return (
+        (month >= start_date.month && year === start_date.year) ||
+        (month <= end_date.month && year === end_date.year)
+      );
+    });
+  }
+
+
   return (
     <CalendarWrapper>
       {months.map(month => (
@@ -24,7 +49,9 @@ const Calendar = props => {
 const mapStateToProps = state => {
   return {
     languages: state.languages,
-    calendar: state.calendar
+    calendar: state.calendar,
+    experience: state.experience,
+    exhibitions: state.exhibitions
   }
 }
 
