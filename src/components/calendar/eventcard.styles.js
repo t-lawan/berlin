7408f0fd07839a10a1bb19650a5accd432f1c732
akinsola <thomas.lawanson@gmail.com>
2019-10-15 12:09:58 +0100

    updated event card

--- src/components/calendar/eventcard.styles.js
+++ src/components/calendar/eventcard.styles.js
@@ -1,12 +1,18 @@
 import styled from "styled-components"
-import { Color, TextSection } from "../../index.styles";
-import AniLink from "gatsby-plugin-transition-link/AniLink";
+import { Color, TextSection } from "../../index.styles"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
+
+export const EventSection = styled(TextSection)`
+  p {
+    font-size: 1em;
+  }
+`
 
 export const EventCardWrapper = styled.div`
   padding: 0.5rem 1rem;
   display: inline;
   :hover {
-    ${TextSection} {
+    ${EventSection} {
       p {
         color: ${Color.red};
       }
@@ -14,9 +20,12 @@ export const EventCardWrapper = styled.div`
   }
 `
 
+
+
 export const EventCardLink = styled(AniLink)`
   text-decoration: none;
 `
 
 export const EventHeading = styled.p`
+  font-size: 1em;
 `
