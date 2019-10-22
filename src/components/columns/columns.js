6d8500582e3297b1c038d67e08011b7c47e4cf8b
akinsola <thomas.lawanson@gmail.com>
2019-10-22 18:27:41 +0100

    added privacy

--- src/components/columns/columns.js
+++ src/components/columns/columns.js
@@ -26,8 +26,8 @@ import FooterComponent from "../footer/footer"
 import NavbarMobile from "../navbar/navbar-mobile"
 import ImageContainer from "../image-container/image-container"
 class Columns extends React.Component {
-  renderedComponents;
-  numberOfColumnsIsTwo = this.props.numberOfColumnsIsTwo;
+  renderedComponents
+  numberOfColumnsIsTwo = this.props.numberOfColumnsIsTwo
   render() {
     if (this.numberOfColumnsIsTwo) {
       this.renderedComponents = (
@@ -46,9 +46,11 @@ class Columns extends React.Component {
 
     return (
       <ColumnsWrapper>
+        {/* First Column */}
         <Column rightBorder={true} hideInMobile>
           <ExperienceController left={true} />
         </Column>
+        {/* Middle Column */}
         <AnimatedColumn
           animationIn={this.props.experience_transition.animationIn}
           animationOut={this.props.experience_transition.animationOut}
@@ -56,41 +58,53 @@ class Columns extends React.Component {
           animateOnMount={false}
           animationInDuration={1000}
           animationOutDuration={200}
+          style={{ zIndex: 0 }}
         >
+          {/* Second Column */}
           <Column rightBorder={true}>
             <StickyTopHeader>
               <ExperienceControllerMobile showInMobile={true} />
               <Header hideInMobile={true} />
             </StickyTopHeader>
             <RelativeHeader>
-              <ImageContainer hideOnHomePage={!this.props.isHome} hideInMobile={true} />
+              <ImageContainer
+                hideOnHomePage={!this.props.isHome}
+                hideInMobile={true}
+              />
             </RelativeHeader>
             <StickyHeader>
               <Navbar hideInMobile={true} />
             </StickyHeader>
 
             {this.renderedComponents}
-            <StickyTopHeader>
+            <StickyFooter showInMobile>
               <DataPrivacy show={!this.props.agreed_to_terms} />
-            </StickyTopHeader>
-            <FixedFooter>
+            </StickyFooter>
+            <FixedFooter showInMobile>
               <FooterComponent />
             </FixedFooter>
             <FixedNavbar>
               <NavbarMobile showInMobile />
             </FixedNavbar>
           </Column>
-
+          {/* Third Column */}
           <Column rightBorder={true} hideInMobile>
             <StickyTopHeader>
               <Jumbotron />
             </StickyTopHeader>
-            {/* <Logo /> */}
             {this.props.thirdColumn}
             <SocialMedia />
           </Column>
-        </AnimatedColumn>
 
+          <StickyFooter hideInMobile>
+            <DataPrivacy show={!this.props.agreed_to_terms} />
+          </StickyFooter>
+
+          <FixedFooter hideInMobile>
+            <FooterComponent />
+          </FixedFooter>
+        </AnimatedColumn>
+        {/* Fourth Column */}
         <Column rightBorder={false} hideInMobile>
           <LanguageController />
           <ExperienceController left={false} />
@@ -105,7 +119,7 @@ Columns.propTypes = {
   secondColumn: PropTypes.node,
   thirdColumn: PropTypes.node,
   numberOfColumnsIsTwo: PropTypes.bool,
-  isHome: PropTypes.bool
+  isHome: PropTypes.bool,
 }
 const mapStateToProps = state => {
   return {
--- src/components/columns/columns.styles.js
+++ src/components/columns/columns.styles.js
@@ -1,5 +1,5 @@
 import styled, { css } from "styled-components"
-import { changeGridToOneRow, size, hideDisplayForTablet } from "../../index.styles"
+import { changeGridToOneRow, size, hideDisplayForTablet, showDisplayForTablet } from "../../index.styles"
 import { Animated } from "react-animated-css"
 
 export const AnimatedColumn = styled(Animated)`
@@ -14,7 +14,13 @@ export const AnimatedColumn = styled(Animated)`
   grid-template-areas: "column column" "footer footer";
   @media (max-width: ${size.mobileM}) {
     /* width: 100%; */
+
     width: 100vw;
+  }
+
+  @media (max-width: ${size.tablet}) {
+    grid-template-rows: auto;
+    grid-template-areas: "column" "footer";
   }
   /* ${changeGridToOneRow} */
 
@@ -58,9 +64,16 @@ export const RelativeHeader = styled.div`
 `
 
 export const StickyFooter = styled.div`
-  z-index: 300;
+  z-index: 500;
   bottom: 0;
   position: sticky;
+  grid-area: footer;
+  @media (max-width: ${size.tablet}) {
+    /* width: 100%; */
+    grid-area: unset;
+  }
+  ${showDisplayForTablet};
+  ${hideDisplayForTablet}
 `
 
 export const FixedFooter = styled.div`
@@ -68,11 +81,19 @@ export const FixedFooter = styled.div`
   bottom: 0;
   position: relative;
   grid-area: footer;
-  ${hideDisplayForTablet};
+  @media (max-width: ${size.tablet}) {
+    grid-area: unset;
+  }
+  ${showDisplayForTablet};
+  ${hideDisplayForTablet}
 `
 
 export const FixedNavbar = styled.div`
   z-index: 600;
   bottom: 0;
   position: sticky;
+  grid-area: footer;
+  @media (max-width: ${size.tablet}) {
+    grid-area: unset;
+  }
 `
