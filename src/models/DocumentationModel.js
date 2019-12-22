74383ba3e98a874a3c3e9e60a3b962cc656e3373
akinsola <thomas.lawanson@gmail.com>
2019-12-22 15:45:31 +0000

    created documentation model

---
+++ src/models/DocumentationModel.js
@@ -0,0 +1,26 @@
+export class DocumentationModel {
+    id;
+    slug;
+    DE;
+    EN;
+    type;
+    related_events;
+    experience;
+    language;
+    audio;
+    video;
+    image_gallery;
+    constructor(id, slug, DE, EN, type, related_events, experience, language, audio, video, image_gallery) {
+        this.id = id;
+        this.slug = slug;
+        this.DE = DE;
+        this.EN = EN;
+        this.type = type;
+        this.related_events = related_events;
+        this.experience = experience;
+        this.language = language;
+        this.audio = audio;
+        this.video =video;
+        this.image_gallery = image_gallery;
+    }
+}
