32c898f6cea29bcdcf7537a39b0fc38adaea6b96
akinsola <thomas.lawanson@gmail.com>
2019-12-22 16:07:50 +0000

    updated document video

--- src/components/documentation/documentation-video.js
+++ src/components/documentation/documentation-video.js
@@ -2,28 +2,69 @@ import React from "react"
 import { getCurrentLanguageString } from "../../utility/helper"
 import { connect } from "react-redux"
 import PropTypes from "prop-types"
-import { PageWrapper, TwoColumnPageWrapper } from "../../templates/page.styles";
+import { PageWrapper, TwoColumnPageWrapper } from "../../templates/page.styles"
+import styled from "styled-components"
 
+const VideoContainer = styled.div`
+  text-align: center;
+  width: 100%;
+`
 const DocumentationVideo = props => {
-  let language = getCurrentLanguageString(props.languages);
-
+  let language = getCurrentLanguageString(props.languages)
+  console.log(props.documentation)
   return (
-      <PageWrapper>
-          <h2> Video </h2>
-          <TwoColumnPageWrapper>
-            <div> 
-                <h2> Text</h2>
-            </div>
-            <div> 
-                <h2> More Text</h2>
-            </div>
-          </TwoColumnPageWrapper>
-      </PageWrapper>
+    <PageWrapper>
+      <VideoContainer
+        dangerouslySetInnerHTML={{
+          __html: props.documentation.video,
+        }}
+      />
+      <TwoColumnPageWrapper>
+        <div>
+          <p> Documentation</p>
+          <p> {content[language].language}: {content[language][props.documentation.language]} </p>
+        </div>
+        <div>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: props.documentation[language].title,
+            }}
+          />
+
+          <div
+            dangerouslySetInnerHTML={{
+              __html: props.documentation[language].subtitle,
+            }}
+          />
+
+          <div
+            dangerouslySetInnerHTML={{
+              __html: props.documentation[language].doc_credits,
+            }}
+          />
+        </div>
+      </TwoColumnPageWrapper>
+    </PageWrapper>
   )
 }
 
 DocumentationVideo.propTypes = {
-    documentation: PropTypes.object
+  documentation: PropTypes.object,
+}
+
+let content = {
+  EN: {
+    language: "Language",
+    en: "English",
+    de: "German",
+    other: "Other",
+  },
+  DE: {
+    language: "Language",
+    en: "English",
+    de: "Deutsch",
+    other: "Other",
+  },
 }
 
 const mapStateToProps = state => {
