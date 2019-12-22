78dd5f5e303dceddd99e5d7375f7e6178ce830cf
akinsola <thomas.lawanson@gmail.com>
2019-12-22 15:45:21 +0000

    created documentation apges for all types

---
+++ src/components/documentation/documentation-image-gallery.js
@@ -0,0 +1,38 @@
+import React from "react"
+import { getCurrentLanguageString } from "../../utility/helper"
+import { connect } from "react-redux"
+import PropTypes from "prop-types"
+import { PageWrapper, TwoColumnPageWrapper } from "../../templates/page.styles";
+
+const DocumentationImageGallery = props => {
+  let language = getCurrentLanguageString(props.languages);
+
+  return (
+      <PageWrapper>
+          <h2> Image Gallery </h2>
+          <TwoColumnPageWrapper>
+            <div> 
+                <h2> Text</h2>
+            </div>
+            <div> 
+                <h2> More Text</h2>
+            </div>
+          </TwoColumnPageWrapper>
+      </PageWrapper>
+  )
+}
+
+DocumentationImageGallery.propTypes = {
+    documentation: PropTypes.object
+}
+
+const mapStateToProps = state => {
+  return {
+    languages: state.languages,
+    documents: state.documents,
+  }
+}
+export default connect(
+  mapStateToProps,
+  null
+)(DocumentationImageGallery)
---
+++ src/components/documentation/documentation-video.js
@@ -0,0 +1,38 @@
+import React from "react"
+import { getCurrentLanguageString } from "../../utility/helper"
+import { connect } from "react-redux"
+import PropTypes from "prop-types"
+import { PageWrapper, TwoColumnPageWrapper } from "../../templates/page.styles";
+
+const DocumentationVideo = props => {
+  let language = getCurrentLanguageString(props.languages);
+
+  return (
+      <PageWrapper>
+          <h2> Video </h2>
+          <TwoColumnPageWrapper>
+            <div> 
+                <h2> Text</h2>
+            </div>
+            <div> 
+                <h2> More Text</h2>
+            </div>
+          </TwoColumnPageWrapper>
+      </PageWrapper>
+  )
+}
+
+DocumentationVideo.propTypes = {
+    documentation: PropTypes.object
+}
+
+const mapStateToProps = state => {
+  return {
+    languages: state.languages,
+    documents: state.documents,
+  }
+}
+export default connect(
+  mapStateToProps,
+  null
+)(DocumentationVideo)
