effaa49b540a52467f7af8c0bc1fc0262cb371d8
Patricia <patricia@leakystudio.com>
2020-01-04 19:58:47 +0100

    removed height 100vh

--- src/components/columns/column.styles.js
+++ src/components/columns/column.styles.js
@@ -5,7 +5,6 @@ import {hideDisplayForTablet } from "../../index.styles";
 export const ColumnLayout = styled.div`
   border-right: ${props => (props.rightBorder ? "1px solid black" : 0)};
   height: 100%;
-  height: 100vh;
   background: white;
   position:relative;
   width:inherit;
