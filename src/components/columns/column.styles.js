b6c402bdb6cc205f269ae7947d9d40336f321b03
Patricia <patricia@leakystudio.com>
2020-01-06 22:57:11 +0100

    CSS Changes

--- src/components/columns/column.styles.js
+++ src/components/columns/column.styles.js
@@ -6,11 +6,19 @@ export const ColumnLayout = styled.div`
   border-right: ${props => (props.rightBorder ? "1px solid black" : 0)};
   height: 100%;
   background: white;
+  z-index:9;
   position:relative;
   width:inherit;
   overflow-y: inherit;
   ${hideDisplayForTablet};
   &::first-of-type{
     border: 0;
+  }
+  :last-child{
+    border-left: solid 1px black;
+  }
+  :nth-child(4){
+    border-right: none;
+    border-top: solid 1px #000;
   }
 `
