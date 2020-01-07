04ae61730934aab85ab345c9320cdae3496fc8a4
akinsola <thomas.lawanson@gmail.com>
2020-01-07 18:25:29 +0000

    created function to truncate text

--- src/utility/helper.js
+++ src/utility/helper.js
@@ -6,6 +6,10 @@ export const getCurrentLanguageString = languageStateObject => {
   }
 }
 
+export const truncateText = (text, number = 25) => {
+  return text.split(' ').slice(0, number).join(' ');
+}
+
 export const pageMap = [
   {EN: "event", DE: "veranstaltung"},
   {EN: "about", DE: "uber"},
