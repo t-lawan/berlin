34af2744815976bcbc550b99b255f3c5d09895b3
Patricia <patricia@leakystudio.com>
2020-01-10 12:38:37 +0100

    Mobile css

--- src/components/events/event-navigator.styles.js
+++ src/components/events/event-navigator.styles.js
@@ -1,11 +1,22 @@
 import styled from "styled-components"
 import AniLink from "gatsby-plugin-transition-link/AniLink";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { size } from "../../index.styles"
 
 export const EventNavigatorWrapper = styled.section`
   display: flex;
   flex-direction: row;
   background: transparent;
+  @media (max-width: ${size.mobileM}) {
+    display:block;
+    padding: 0.5em 0 0 0.7em;
+    width:100%;
+    :after {
+      content:"";
+      clear:both;
+      display:table;
+    }
+  }
 `
 
 export const EventNavigatorButton = styled(AniLink)`
@@ -13,6 +24,12 @@ export const EventNavigatorButton = styled(AniLink)`
   line-height:1;
   padding: 0.5em 0 0 1em;
   text-decoration: none;
+  @media (max-width: ${size.mobileM}) {
+    margin-bottom: 0.6em;
+    margin-top: -0.5em;
+    font-size: 1.55em;
+    padding: 0 1em 0 0;
+  }
 `
 
 export const EventNavigatorIcon = styled(FontAwesomeIcon)`
