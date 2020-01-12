7d4022b1a367ac6540671d81545dc9c362871386
akinsola <thomas.lawanson@gmail.com>
2020-01-12 15:54:00 +0000

    updated resource

--- src/utility/helper.js
+++ src/utility/helper.js
@@ -23,7 +23,13 @@ export const pageMap = [
 ]
 
 export const transitionBackground = "url(https://11.berlinbiennale.de/wp-content/themes/bb11-car-trans2/images/waitaminute1.gif) center no-repeat fixed white"
-
+export const shuffle = (a) => {
+  for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
 export const createPath = (language, path) => {
   let prePath = pageMap.find((pageType) => {
     let type = path.split('/')[0].toLowerCase();
