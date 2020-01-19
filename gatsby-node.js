b572fe57fc998456309ee4c7766f087f3cbe0a8e
Patricia <patricia@leakystudio.com>
2020-01-19 17:02:39 +0100

    Added new Data fields

--- gatsby-node.js
+++ gatsby-node.js
@@ -345,12 +345,22 @@ exports.createPages = async ({ graphql, actions }) => {
             acf {
               exhibition_floorplan
               active_exhibition
+              temporary_exp_page
               use_gallery_images
               exhibition_venue {
                 wordpress_id
               }
               exp_number
               start_date
+              exp_open_days {
+                monday
+                tuesday
+                wednesday
+                thursday
+                friday
+                saturday
+                sunday
+              }
               DE {
                 description
                 subtitle
@@ -363,6 +373,7 @@ exports.createPages = async ({ graphql, actions }) => {
                 exp_bb11_right_header
                 promotional_sticker_for_homepage
                 promotional_sticker_url
+                german_page_slug
               }
               EN {
                 description
