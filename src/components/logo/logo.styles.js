d76b452ec634a0361d92159e8f057f6016b9f79e
Patricia <patricia@leakystudio.com>
2020-01-20 12:54:43 +0100

    Changed position to sticky

--- src/components/logo/logo.styles.js
+++ src/components/logo/logo.styles.js
@@ -3,11 +3,12 @@ import styled from "styled-components"
 export const LogoWrapper = styled.div`
   display: block;
   margin: auto;
+  position: sticky;
+  top: 0;
   z-index: 0;
   padding: 0em 1em;
   background: white;
   max-height:500px;
-  border-bottom:solid 1px black;
   >img {
   	max-width:310px;
   	margin-left: auto;
