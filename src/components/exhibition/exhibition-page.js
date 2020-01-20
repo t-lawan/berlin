058a6773425ed2a8ba5084fe2004373b2642e542
akinsola <thomas.lawanson@gmail.com>
2020-01-20 23:21:08 +0000

    updated exhibition page

--- src/components/exhibition/exhibition-page.js
+++ src/components/exhibition/exhibition-page.js
@@ -3,10 +3,13 @@ import React from "react"
 import styled from "styled-components"
 import { connect } from "react-redux"
 import { size } from "../../index.styles"
+import { getDocument } from "../../store/selector"
+import { getCurrentLanguageString } from "../../utility/helper";
 
 const ExhibitionPageWrapper = styled.div`
   z-index: 6000;
   left: 5%;
+  padding: 1rem;
   width: calc(100% - 10%);
   height: 100vh;
   position: fixed;
@@ -17,17 +20,28 @@ const ExhibitionPageWrapper = styled.div`
     left: 0;
     bottom: 45px;
     height: calc(100vh - 91px);
-    top:45px;
+    top: 45px;
   }
 `
+
+const ExhibitionPageImage = styled.img`
+  width: 30%;
+`
 class ExhibitionPage extends React.Component {
+  language;
+  image;
   render() {
-    let exhibition = this.props.exhibitions.find((ex) => {
-        return ex.experience == this.props.experience;
+    let exhibition = this.props.exhibitions.find(ex => {
+      return ex.experience == this.props.experience
     })
+
+    this.language = getCurrentLanguageString(this.props.languages);
+
+    this.image = getDocument(this.props.documents, exhibition[this.language].temp_exp_graphic)
+
     return (
       <ExhibitionPageWrapper show={exhibition.temporary_uploaded}>
-        <p> eh exhibition no work (spanish accent)</p>
+        {this.image ? <ExhibitionPageImage src={this.image.url} /> : null}
       </ExhibitionPageWrapper>
     )
   }
@@ -37,6 +51,8 @@ const mapStateToProps = state => {
   return {
     experience: state.experience,
     exhibitions: state.exhibitions,
+    documents: state.documents,
+    languages: state.languages
   }
 }
 
