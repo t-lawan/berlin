b52f5e8184bbdce2637f336bc6b6565026e1580e
akinsola <thomas.lawanson@gmail.com>
2020-02-24 21:13:24 +0000

    update

--- src/components/events/upcomingevents.js
+++ src/components/events/upcomingevents.js
@@ -61,7 +61,7 @@ const UpcomingEvents = props => {
       </p>
       {filteredItems.map(item => (
         <EventItem isActive={currentExhibition.active} key={item.id}>
-          <EventLink fade onClick={() => props.startTransition()} to={createPath(language, `${item.slug}`)}>
+          <EventLink onClick={() => props.startTransition()} to={createPath(language, `${item.slug}`)}>
             {/* <EventLink
             cover
             direction="down"
