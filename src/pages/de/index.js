eab8cd9fd6a555b30de86bc17efc23381f14af71
Patricia <patricia@leakystudio.com>
2020-02-24 20:54:33 +0100

    added margin top to display more animation image

--- src/pages/de/index.js
+++ src/pages/de/index.js
@@ -13,8 +13,12 @@ import Logo from "../../components/logo/logo";
 
 export const IndexPageWrapper = styled.div`
   padding: 0em;
+  @media (max-width: ${size.mobileS}) {
+    margin-top:95px;
+    background:#FFF;
+  }
   @media (max-width: ${size.mobileM}) {
-    margin-top:75px;
+    margin-top:140px;
     background:#FFF;
   }
 `
