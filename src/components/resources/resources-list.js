593cb9bb5e768589aaa4f4e0576357d6959042d1
Patricia <patricia@leakystudio.com>
2020-02-27 21:38:06 +0100

    Font Scaling

--- src/components/resources/resources-list.js
+++ src/components/resources/resources-list.js
@@ -22,34 +22,34 @@ const ResourcesListWrapper = styled.div`
 `
 
 const ResourceTitle = styled.p`
-  font-size: 1rem;
+  font-size: 1em;
   transition:all 0.2s ease-in-out;
   :last-child {
     margin-bottom:0;
   }
   @media (max-width: ${size.mobileM}) {
-    font-size:1.0rem;
+    font-size:1em;
     margin-bottom:0.3em;
   }
   @media (min-width: ${size.mobileL}) {
-    font-size:0.95rem;
+    font-size:0.95em;
   }
   @media (min-width: ${size.laptop}) {
-    font-size:1.03rem;
+    font-size:1em;
   }
 `
 
 const ResourceLabel = styled.p`
-  font-size: 1rem;
+  font-size: 1em;
   margin: 0.5em 0 0;
   @media (max-width: ${size.mobileM}) {
-    font-size:1.0rem;
+    font-size:1.0em;
   }
   @media (min-width: ${size.mobileL}) {
-    font-size:0.95rem;
+    font-size:0.95em;
   }
   @media (min-width: ${size.laptop}) {
-    font-size:1rem;
+    font-size:1em;
   }
 `
 
@@ -90,7 +90,12 @@ const TextBox = styled.div`
   @media (max-width: ${size.tablet}) {
     padding: 0.7em 0.7em;
   }
-  
+  > p:first-child {
+    margin-bottom: 0;
+  }
+  > p:nth-child(2) {
+    margin-top: 0.2em;
+  }
 `
 const ResourcesList = props => {
   let language = getCurrentLanguageString(props.languages)
