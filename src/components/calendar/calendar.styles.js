d12fa99c3b12fc510212cd37adeb8119604d3521
akinsola <thomas.lawanson@gmail.com>
2020-03-02 21:28:34 +0000

    added scroll area

--- src/components/calendar/calendar.styles.js
+++ src/components/calendar/calendar.styles.js
@@ -8,10 +8,15 @@ import {
 export const CalendarWrapper = styled.div`
 display: grid;
 grid-template-columns: 1fr;
+ overflow-y: scroll;
 @media (max-width: ${size.mobileM}) {
     background:#FFF;
     }
 /* grid-template-columns: 1fr 1fr 1fr 1fr;
 ${changeGridToThreeEqualRows};
 ${changeGridToTwoEqualColumns}; */
+`
+
+export const CalendarScrollArea = styled.div`
+
 `
