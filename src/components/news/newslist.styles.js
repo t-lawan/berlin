e4716eba2385b5f6c63b69cd2623bbf7654edce0
Patricia <patricia@leakystudio.com>
2020-04-07 13:52:22 +0200

    Mobile News Styling

--- src/components/news/newslist.styles.js
+++ src/components/news/newslist.styles.js
@@ -1,13 +1,17 @@
 import styled from "styled-components"
-import { Color } from "../../index.styles";
+import { Color, size } from "../../index.styles";
 
 export const NewsListWrapper = styled.div`
   /* padding: 0.7em 2em; */
   background: ${Color.yellow};
-  position:relative;
   z-index:1;
   border-top: ${props => props.isHome ? 'solid 1px #000': 'none'} ;
-  margin-bottom: -1px;
+  margin-bottom: ${props => props.isCurrent ? '0' : '-1px'};
   display: ${props => props.show ? 'inherit' : 'none'};
   position: relative;
+  @media (max-width: ${size.mobileM}) {
+    width: calc(100% + 1.4em);
+    margin-left: -0.7em;
+    margin-top: ${props => props.isCurrent ? '-1.7em' : '0'};
+  }
 `
