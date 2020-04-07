4acf67c49e65af249849c429e96413d7a3cbbda8
Patricia <patricia@leakystudio.com>
2020-04-07 22:56:22 +0200

    Event Nav spacing

--- src/components/events/event-navigator.styles.js
+++ src/components/events/event-navigator.styles.js
@@ -24,13 +24,13 @@ export const EventNavigatorWrapper = styled.section`
     padding: 0.7em 0 0 0.7em;
   }
   @media (min-width: ${size.laptop}) {
-    padding: 0.15em 0.7em 1em 1em;
+    padding: 0.15em 0.7em 1.5em 1em;
   }
   @media (min-width: ${size.laptopM}) {
-    padding: 0.35em 0.7em 1.25em 1em;
+    padding: 0.35em 0.7em 1.8em 1em;
   }
   @media (min-width: ${size.laptopL}) {
-    padding: 0.5em 0.7em 1.05em 1em;
+    padding: 0.5em 0.7em 1.9em 1em;
   }
 `
 
