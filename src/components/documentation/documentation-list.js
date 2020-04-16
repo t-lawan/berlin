801936391e496347297540db69475b7be4515f49
akinsola <thomas.lawanson@gmail.com>
2020-04-16 16:06:15 +0100

    filter based on list_on_media_overview

--- src/components/documentation/documentation-list.js
+++ src/components/documentation/documentation-list.js
@@ -54,7 +54,9 @@ let content = {
 
 const DocumentationList = props => {
   let language = getCurrentLanguageString(props.languages)
-  let documentation = props.documentation
+  let documentation = props.documentation.filter((d) => {
+    return d.list_on_media_overview;
+  })
 
   const createComponent = (doc, index) => {
     let renderComponent;
@@ -83,8 +85,6 @@ const DocumentationList = props => {
           </DocumentationItem>
         </DocumentationLink>
       )
-    
-
     return renderComponent
   }
   return (
