1181b47402ab0e612425b3e55310e4f7d766c9b4
akinsola <thomas.lawanson@gmail.com>
2020-05-10 11:57:28 +0100

    trigger is viewing upon mounting

--- src/components/page-loader/page-loader.js
+++ src/components/page-loader/page-loader.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux"
 import styled from "styled-components"
 import PageLoaderLogo from "../../images/transition.gif"
 import { Color } from "../../index.styles"
-import { setFreshLoadToFalse } from "../../store/action"
+import { setFreshLoadToFalse, isViewing } from "../../store/action"
 
 const PageLoaderWrapper = styled.div`
   background: ${Color.yellow};
@@ -26,6 +26,9 @@ const PageLoaderImage = styled.img`
 class PageLoader extends React.Component {
     componentDidMount() {
       this.hidePageLoader()
+      setTimeout(() => {
+        this.props.isViewing()
+      }, 1000)
     }
 
     hidePageLoader = () => {
@@ -51,12 +54,14 @@ class PageLoader extends React.Component {
 const mapStateToProps = state => {
   return {
     freshLoad: state.freshLoad,
+    is_viewing: state.is_viewing
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
     setFreshLoadToFalse: () => dispatch(setFreshLoadToFalse()),
+    isViewing: () => dispatch(isViewing())
   }
 }
 
