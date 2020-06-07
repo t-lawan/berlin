7347d1f7f5891d31dc951d6222ad4ed745b6d198
Patricia <patricia@leakystudio.com>
2020-06-07 14:02:48 +0200

    subpage Translations

---
+++ src/components/practical-information/practical-information-accommodation.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { connect } from "react-redux"
+import { PageTitle } from "../../templates/page.styles";
+import { getCurrentLanguageString } from "../../utility/helper";
+
+const PracticalInformationAccommodation = props => {
+    let language = getCurrentLanguageString(props.languages)
+
+    return (
+        <>
+            <PageTitle> {page_title[language].title}</PageTitle>
+            <p> accommodation text</p>
+        </>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+      languages: state.languages,
+    }
+  }
+  
+  let page_title = {
+    EN: {
+      title: "accommodation",
+    },
+    DE: {
+      title: "unterkunft",
+    },
+  }
+  
+  export default connect(mapStateToProps, null)(PracticalInformationAccommodation)
+  
