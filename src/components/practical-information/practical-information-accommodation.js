359f040b52c657078640e8108bc89410094d390e
akinsola <thomas.lawanson@gmail.com>
2020-07-26 11:46:16 +0100

    updated accomidation component

--- src/components/practical-information/practical-information-accommodation.js
+++ src/components/practical-information/practical-information-accommodation.js
@@ -1,34 +1,38 @@
 import React from "react"
 import { connect } from "react-redux"
-import { PageTitle } from "../../templates/page.styles";
-import { getCurrentLanguageString } from "../../utility/helper";
+import { PageTitle } from "../../templates/page.styles"
+import { getCurrentLanguageString } from "../../utility/helper"
+import { PracticalInformationContentBlock } from "./practical-information-admission";
 
 const PracticalInformationAccommodation = props => {
-    let language = getCurrentLanguageString(props.languages)
+  let language = getCurrentLanguageString(props.languages)
+  let content = props.content.acf[language]
 
-    return (
-        <>
-            <PageTitle> {page_title[language].title}</PageTitle>
-            <p> accommodation text</p>
-        </>
-    )
+  return (
+    <>
+      <PageTitle> {page_title[language].title}</PageTitle>
+      {content.content_block
+        ? content.content_block.map((item, index) =>
+            PracticalInformationContentBlock(item, index)
+          )
+        : null}
+    </>
+  )
 }
 
 const mapStateToProps = state => {
-    return {
-      languages: state.languages,
-    }
+  return {
+    languages: state.languages,
   }
-  
-  let page_title = {
-    EN: {
-      title: "accommodation",
-    },
-    DE: {
-      title: "unterkunft",
+}
 
-    },
-  }
-  
-  export default connect(mapStateToProps, null)(PracticalInformationAccommodation)
-  
+let page_title = {
+  EN: {
+    title: "accommodation",
+  },
+  DE: {
+    title: "unterkunft",
+  },
+}
+
+export default connect(mapStateToProps, null)(PracticalInformationAccommodation)
