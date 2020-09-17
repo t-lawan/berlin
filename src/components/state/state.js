61a0a26c506b543ffe5ccbe231562a80f962d39c
akinsola <thomas.lawanson@gmail.com>
2020-09-17 22:19:34 +0100

    update

--- src/components/state/state.js
+++ src/components/state/state.js
@@ -365,7 +365,7 @@ const State = props => {
                 slug
                 localFile {
                   childImageSharp {
-                    fluid(quality: 90, maxWidth: 1000) {
+                    fluid(quality: 90, maxWidth: 600) {
                       aspectRatio
                       src
                       srcSet
