76237de2d94ee0e081091a25f6f09ae312bedc11
Patricia <patricia@leakystudio.com>
2020-10-31 17:52:54 +0100

    Dimension changes for Mobile/Tablet

--- src/components/exchange/exchange-page.js
+++ src/components/exchange/exchange-page.js
@@ -2,33 +2,66 @@ import React from "react"
 import { connect } from "react-redux"
 import { getCurrentLanguageString } from "../../utility/helper"
 import { ContentBlock, PageTitle } from "../../templates/page.styles"
-import { ExchangeContentBlock } from "./exchange-curatorial-workshop";
+import {
+  ExchangeContentBlock,
+  ContentBlockWrapper,
+} from "./exchange-curatorial-workshop"
+import ImageGalleryResource from "../../partials/ImageGalleryResource"
+import styled from 'styled-components';
+import { size } from "../../index.styles";
 
+
+const ImageWrapper = styled.div`
+  width: 93vw;
+  @media (min-width: ${size.mobileL}) {
+    width: 94vw;
+  }
+  @media (min-width: ${size.tablet}) {
+    width: 64vw;
+  }
+  @media (min-width: ${size.tabletL}) {
+    width: 45vw;
+  }
+`; 
 const ExchangePage = props => {
-    let language = getCurrentLanguageString(props.languages)
-    let content = props.content.acf[language]
-    return (
-        <>
-            <PageTitle> {page_title[language].title}</PageTitle>
-            {content.content_block ? content.content_block.map((item, index) => ExchangeContentBlock(item, index)) : null}
-        </>
-    )
+  let language = getCurrentLanguageString(props.languages)
+  let content = props.content.acf[language]
+  let image_gallery
+  if (props.content.acf.image_gallery) {
+    image_gallery = props.content.acf.image_gallery.map(image => {
+      return image.wordpress_id
+    })
+  }
+  return (
+    <div>
+      <PageTitle> {page_title[language].title}</PageTitle>
+      {image_gallery ? (
+        <ImageWrapper>
+          <ImageGalleryResource ids={image_gallery} />{" "}
+        </ImageWrapper>
+      ) : null}
+      {content.content_block
+        ? content.content_block.map((item, index) =>
+            ExchangeContentBlock(item, index)
+          )
+        : null}
+    </div>
+  )
 }
 
 const mapStateToProps = state => {
-    return {
-      languages: state.languages,
-    }
+  return {
+    languages: state.languages,
   }
-  
-  let page_title = {
-    EN: {
-      title: "exchange",
-    },
-    DE: {
-      title: "austausch",
+}
 
-    },
-  }
-  
-  export default connect(mapStateToProps, null)(ExchangePage)
+let page_title = {
+  EN: {
+    title: "exchange",
+  },
+  DE: {
+    title: "austausch",
+  },
+}
+
+export default connect(mapStateToProps, null)(ExchangePage)
