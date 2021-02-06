abe698b9e1e2a516ef4a074724a20b8af02228a2
akinsola <thomas.lawanson@gmail.com>
2021-02-06 10:40:27 +0000

    update

--- src/components/events/upcomingevents.styles.js
+++ src/components/events/upcomingevents.styles.js
@@ -20,48 +20,19 @@ export const EventsWrapper = styled.div`
 
 export const EventItem = styled.div`
   color: black;
-  background-color:#FFF;
-  position:relative;
-  &:first-of-type {
-    border: ${props => props.isActive ? `4px solid ${Color.yellow}` : ''};
-    @media (max-width: ${size.mobileL}) {
-      border: ${props => props.isCurrent ? `4px solid ${Color.yellow}` : ''};
-      border-bottom: ${props => props.isActive ? `4px solid ${Color.yellow}` : ''} !important;
-      border-left: ${props => props.isActive ? `4px solid ${Color.yellow}` : ''} !important;
-    } 
-    @media (min-width: ${size.tablet}) {
-    padding: ${props => props.isActive ? `1em 0.7em` : ''};
-    border-bottom: ${props => props.isActive ? `4px solid ${Color.yellow}` : ''} !important;
-    }
-    @media (min-width: ${size.laptop}) {
-      padding: ${props => props.isActive ? `1.1em 1em` : ''};
-    }
-    @media (min-width: ${size.laptopM}) {
-      padding: ${props => props.isActive ? `1.3em 2em` : ''};
-    }
-  };
-  
-  &:first-of-type:after {
-    content:" ";
-    position:absolute;
-    display:block;
-    bottom:-4px;
-    height:1px;
-    left:-4px;
-    width:calc(100% + 8px);
-    background:#000;
-  };
+  background-color: #FFF;
+  position: relative;
   :last-child {
-    border-bottom:none;
+    border-bottom: none;
   }
-  p{
-    margin-bottom:0;
+  p {
+    margin-bottom: 0;
     line-height: 1.3;
   }
   border-bottom: 1px solid black;
   @media (max-width: ${size.mobileM}) {
     padding: 1em 1em;
-    p{
+    p {
     font-size: 1em;
     }
   }
@@ -73,20 +44,20 @@ export const EventItem = styled.div`
   }
   @media (min-width: ${size.tablet}) {
     padding: 1em 1em;
-    p{
+    p {
     font-size: 0.95em;
     }
   }
   @media (min-width: ${size.laptop}) {
     padding: 1.2em 1.3em;
-    p{
+    p {
     font-size: 1em;
     line-height: 1.3;
     }
   }
   @media (min-width: ${size.laptopM}) {
     padding: 1.3em 2.3em;
-    p{
+    p {
     font-size: 1em;
     line-height: 1.35;
     }
@@ -101,7 +72,6 @@ export const EventLink = styled(UnderlineSectionLink)`
     font-size: 1em;
   }
   @media (max-width: ${size.mobileM}) {
-    
     line-height: 1.3;
   }
   /* padding: 1rem; */
